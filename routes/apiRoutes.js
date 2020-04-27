751f55f525906ddb3b116db8c4e7d7b4a7cb86a7
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-27 19:43:46 -0400

    fixing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,24 @@
-const db = require("./models");
+const db = require("../model");
 const router = require("express").Router();
 
+router.get("/workouts", (req,res)=>{
+    db.Cardio.find({})
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+      console.log(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+    db.Resistance.find({})
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+      console.log(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+})
 
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,11 @@
-const db = require("./models");
+const db = require("../model");
 const router = require("express").Router();
+const path = require("path");
 
+//exercise route page
+
+router.get("/exercise", (req, res) => res.sendFile(path.join(__dirname, "../public/exercise.html"))
+);
 
 
 
